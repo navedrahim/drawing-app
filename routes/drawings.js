9a43f74281ee6c87f8f5cd342c0b9c3c03716515
navedrahim <nrah1988@gmail.com>
2021-11-17 12:09:54 -0500

    added and tested endpoints

--- routes/drawings.js
+++ routes/drawings.js
@@ -4,5 +4,9 @@ import * as controllers from '../controllers/drawings.js'
 const router = Router()
 
 router.get('/drawings', controllers.getDrawings)
+router.get('/drawings/:id', controllers.getDrawing)
+router.post('/drawings', controllers.createDrawing)
+router.put('/drawings/:id', controllers.updateDrawing)
+router.delete('/drawings/:id', controllers.deleteDrawing)
 
 export default router
