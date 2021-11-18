0cc36df4a79f5c25b49c16d4144d84233f226e52
Nicholas Rynearson <nrynearson@gmail.com>
2021-11-18 13:27:16 -0500

    sets up basic color picking functionality

--- client/src/components/Hole/Hole.jsx
+++ client/src/components/Hole/Hole.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react"
 import "./Hole.css"
 
 function Hole({ selectedColor }){
+  const [pegColor, setPegColor] = useState("")
+  const [colorSet, setColorSet] = useState(false)
+  const handleClick = () => {
+    setPegColor(selectedColor)
+    setColorSet(true)
+  }
+  const handleMouseEnter = () => {
+    if (!colorSet) setPegColor(selectedColor)
+  }
+
+  const handleMouseLeave = () => {
+    if (!colorSet) setPegColor("black")
+  }
   return (
-    <div style={{backgroundColor: selectedColor}} className="hole">
+    <div onClick={handleClick} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={{backgroundColor: pegColor}} className="hole">
 
     </div>
   )
