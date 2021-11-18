6d4a53c333f18ca554e9336ef80c4bc22ae8c80b
Nicholas Rynearson <nrynearson@gmail.com>
2021-11-18 13:02:09 -0500

    progress on Row, Hole, Canvas components

---
+++ client/src/components/Hole/Hole.jsx
@@ -0,0 +1,11 @@
+import "./Hole.css"
+
+function Hole({ selectedColor }){
+  return (
+    <div style={{backgroundColor: selectedColor}} className="hole">
+
+    </div>
+  )
+}
+
+export default Hole
