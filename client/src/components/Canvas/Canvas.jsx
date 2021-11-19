9c4cf727efe9beb917e86e2a3484d6a51a2809b8
Nicholas Rynearson <nrynearson@gmail.com>
2021-11-18 20:30:55 -0500

    makes progress on reset; adds light effect; basic styling on DrawingCanvas screen

--- client/src/components/Canvas/Canvas.jsx
+++ client/src/components/Canvas/Canvas.jsx
@@ -1,10 +1,10 @@
 import Row from "../Row/Row.jsx"
 import "./Canvas.css"
 
-function Canvas({ selectedColor, reset }){
+function Canvas({ selectedColor, reset, light }){
   const rows = []
   for (let i=0; i < 20; i++){
-    rows.push(<Row key={i} selectedColor={selectedColor} reset={reset}/>)
+    rows.push(<Row key={i} selectedColor={selectedColor} reset={reset} light={light}/>)
   }
   return (
     <div className="canvas">
--- client/src/screens/DrawingCanvas/DrawingCanvas.jsx
+++ client/src/screens/DrawingCanvas/DrawingCanvas.jsx
@@ -1,28 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { CompactPicker } from "react-color";
 import Canvas from "../../components/Canvas/Canvas";
 import "./DrawingCanvas.css";
 function DrawingCanvas() {
   const [selectedColor, setColor] = useState("");
   const [reset, setReset] = useState(false);
+  const [light, toggleLight] = useState(false)
   const changeColor = (color) => {
     setColor(color.hex);
     setReset(false);
   };
+  const handleLight = () => {
+    toggleLight(!light)
+  }
   const handleReset = () => {
     setReset(true);
+    setTimeout(() => setReset(false), 10)
   };
+  
+
   return (
     <div className="drawing-canvas">
       <div className="drawing-container">
         <div className="drawing-area">
-          <Canvas selectedColor={selectedColor} reset={reset} />
+          <Canvas selectedColor={selectedColor} reset={reset} light={light} />
         </div>
+        <div className="tools-area">
         <div className="color-picker">
           <CompactPicker color={selectedColor} onChangeComplete={changeColor} />
         </div>
-        <div className="reset-button">
+        <div className="button-container">
+          <button onClick={handleLight} style={{backgroundColor: light ? "yellow" : "gray"}}>Light</button>
           <button onClick={handleReset}>Reset</button>
+        </div>
         </div>
       </div>
     </div>
