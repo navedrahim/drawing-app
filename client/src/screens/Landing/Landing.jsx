ee091681961c1570a9745d8dae9b278c6550042e
Nicholas Rynearson <nrynearson@gmail.com>
2021-11-19 12:36:15 -0500

    starts layout for Landing page with minimal CSS styling

---
+++ client/src/screens/Landing/Landing.jsx
@@ -0,0 +1,46 @@
+import "./Landing.css"
+
+function Landing(){
+  return(
+    <div className="langing-page">
+      <header className="landing-header">
+        <h1>DrawBrite</h1>
+        <p className="brief-description">
+          Watch your drawings come to life with the magic of DrawBrite!
+        </p>
+      </header>
+      <main>
+        <h3 className="heading">Heading Text</h3>
+        <p className="description">
+          give me all your money
+        </p>
+        <div className="video-conainter">
+          {/* video element */}
+        </div>
+      </main>
+      <div className="meet-the-engineers">
+        <p className="meet-section">
+          Meet the engineers:
+        </p>
+        <div className="card-container">
+        <div className="card">
+          <h3 className="name">Naved Rahim</h3>
+          <p></p>
+          <img src="https://i.imgur.com/XQv7eEy.png?1" alt="Naved" />
+          <div className="links">
+          </div>
+        </div>
+        <div className="card">
+          <h3 className="name">Nicholas Rynearson</h3>
+          <p></p>
+          <img src="https://i.imgur.com/kHvKSSc.jpg" alt="Nicholas" />
+          <div className="links">
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Landing
